07e65ea35c5a93a4cc0346a0db0567f39cd0c8bd
Rashid Abubakar <rashidsamadina@gmail.com>
2026-04-18 18:49:38 +0300

    conf: configure database url

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,9 @@
 from sqlmodel import SQLModel, Session, create_engine
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 def create_db_and_Tables():
     SQLModel.metadata.create_all(engine)
