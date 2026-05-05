8b49092e366a0447a53d53b7e34b5a126ffb429e
ABDIRASHID <rashidsamadina@gmail.com>
2026-05-06 01:51:14 +0300

    db: add concurrent indexes to profiles table
    - Added  to optimize query performance.
    - Includes composite index on (country, gender, age).
    - Includes unique index on lowercase name for native deduplication during ingestion.

--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,15 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, 
+    pool_size=5,
+    max_overflow=5,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+    echo=True
+)
 
 def create_db_and_Tables():
     SQLModel.metadata.create_all(engine)
