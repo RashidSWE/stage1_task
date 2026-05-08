e13f78c8d8f95d0db49bdda29e451edb8820a60d
ABDIRASHID <rashidsamadina@gmail.com>
2026-05-09 01:52:58 +0300

    Implement: add get_user_browser to authenticate user

--- app/services/security.py
+++ app/services/security.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime, timedelta, timezone
 from fastapi import Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Request, HTTPException, status
 
 JWT_SECRET = os.getenv("JWT_SECRET", "secret_key")
 ALGORITHM = "HS256"
@@ -16,11 +17,11 @@ def create_access_token(user_id: str ) -> str:
 
 def create_refresh_token(user_id: str) -> str:
     """Expires in a day"""
-    expire = datetime.now(timezone.utc) ++ timedelta(days=1)
+    expire = datetime.now(timezone.utc) + timedelta(days=1)
     payload = {"sub": user_id, "exp": expire, "type": "refresh"}
     return jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHM)
 
-def get_current_user(credentials: HTTPAuthorizationCredentilas = Security(security)):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Security(security)):
     token = credentials.credentials
 
     try:
@@ -32,4 +33,32 @@ def get_current_user(credentials: HTTPAuthorizationCredentilas = Security(securi
         raise HTTPException(status_code=401, detail="Token Expired")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
-    
+
+
+async def get_user_browser(request: Request):
+
+    token = request.cookies.get("access_token")
+    csrf_cookie = request.cookies.get("csrf_token")
+    csrf_header = request.headers.get("X-CSRF-Token")
+
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Unauthorized: No web session found."
+        )
+
+    if not csrf_cookie or not csrf_header or csrf_cookie != csrf_header:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, 
+            detail="Forbidden: CSRF validation failed."
+        )
+
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
+        return payload.get("sub")
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token Expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
